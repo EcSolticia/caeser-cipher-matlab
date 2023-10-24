e5cf08d40a751e89414b93ee64869246da4e5e13
Logielle Entiopya <137521060+logielle@users.noreply.github.com>
2023-10-24 16:48:01 +0600

    Finalized the encryption function (Updated CaeserEncr.m)

--- CaeserEncr.m
+++ CaeserEncr.m
@@ -1,14 +1,14 @@
-plaintext = 'TEST MESSAGE';
+
+function [cipherStr] = CaeserEncr(plaintext)
 
 na = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '];
 nac = ['C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', 'A', 'B'];
-
+    
 S = size(plaintext);
 SIZE = S(2);
 
 cipher = [];
 SIZE_cipher = 0;
-pcipher = [];
 
 %%converts the list of numbers to an array
 for i = 1:SIZE %%i is the ith char
@@ -19,13 +19,11 @@ for i = 1:SIZE %%i is the ith char
             SIZE_cipher = s(2);
             
             cipher(SIZE_cipher + 1) = j;
-            SIZE_cipher = SIZE_cipher + 1;
-            
+            SIZE_cipher = SIZE_cipher + 1;  
         end
     end
 end
 
-%%reconstructs the message from the list of numbers
 cipherStr = '';
 
 for i = 1:SIZE_cipher
@@ -33,4 +31,4 @@ for i = 1:SIZE_cipher
     cipherStr(s(2) + 1) = nac(cipher(i));
 end
 
-cipherStr
+end
